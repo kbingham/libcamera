1a4248b1c01ed3e5a5f8264701e6016a884a69fd
Kieran Bingham <kieran.bingham@ideasonboard.com>
2020-09-30 13:43:18 +0100

    test: cam: valgrind updates

--- test/cam/capture-test.sh
+++ test/cam/capture-test.sh
@@ -6,33 +6,66 @@ TestPass=0
 TestFail=255
 TestSkip=77
 
+# Initialise success, set for failure.
+ret=$TestPass
+
+ok() {
+	echo "ok $*"
+}
+
+nok() {
+	echo "not ok $*"
+	ret=$TestFail
+}
+
+valgrind=$(command -v valgrind)
+
+if [ x"" = x"$valgrind" ] ; then
+	echo "skip 1 - Valgrind unavailable ..."
+	exit $TestSkip
+fi
+
 # Tests expect to be run from the meson.project_build_root()
 cam=${1:-src/cam/cam}
 
 if [ ! -e "$cam" ] ; then
-	echo "Failed to find cam utility."
+	nok "1 - failed to find cam utility."
 	exit $TestFail
 fi
 
 # Unfortunately, we don't have a 'machine interface', so we rely on parsing the
 # output of cam...
-num_cameras=$("$cam" -l | grep -cv "Available")
+num_cameras=$("$cam" -l | grep -v "Available" | wc -l)
 
-if [ "$num_cameras" -lt 1 ];
-then
-	echo "Warning: No cameras available"
-	exit $TestSkip
-fi
+# Enter TAP plan
+echo "1..$num_cameras"
 
-## Run for each camera detected
 for i in $(seq 1 1 "$num_cameras");
 do
 	"$cam" -c "$i" -C10
 	ret=$?
 	if [ $ret != 0 ] ; then
-		echo "$i - $cam returned $ret"
+		nok "$i - $cam returned $ret"
 		continue
 	fi
+
+	log_file="valgrind-cam-$i.log"
+	"$valgrind" "$cam" -c "$i" -C10 > "$log_file" 2>&1
+	ret=$?
+	if [ $ret != 0 ] ; then
+		nok "$i - $valgrind returned $ret"
+		continue
+	fi
+
+	# I'd prefer a better way of checking there are no leaks, as well as reporting
+	# the different categories from valgrind as distinct tests.
+	if ! grep "no leaks are possible" "$log_file" > /dev/null; then
+		nok "$i - Valgrind Errors detected"
+		cat $log_file > /dev/stderr
+		continue
+	fi
+
+	ok "$i - Camera $i reports no leaks"
 done;
 
-exit $TestPass
+exit $ret
