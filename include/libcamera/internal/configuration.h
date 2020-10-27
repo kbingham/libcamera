b0405117f50dff96267cbaeabbf65c5c825573f4
Kieran Bingham <kieran.bingham@ideasonboard.com>
2020-10-27 14:03:43 +0000

    libcamera: Add configuration interface
    Provide an interface to support reading configuration files.
    
    Initial support is included for JSON formatted files, but extending this
    with other configuration file formats is not excluded.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ include/libcamera/internal/configuration.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2020, Google Inc.
+ *
+ * configuration.h - Parsing configuration files
+ */
+#ifndef __LIBCAMERA_INTERNAL_CONFIGURATION_H__
+#define __LIBCAMERA_INTERNAL_CONFIGURATION_H__
+
+namespace libcamera {
+
+class Configuration
+{
+public:
+	Configuration();
+};
+
+} /* namespace libcamera */
+
+#endif /* __LIBCAMERA_INTERNAL_CONFIGURATION_H__ */
+
---
+++ src/libcamera/configuration.cpp
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2020, Google Inc.
+ *
+ * configuration.cpp - Parsing configuration files
+ */
+#include "libcamera/internal/configuration.h"
+
+#include "libcamera/internal/file.h"
+#include "libcamera/internal/log.h"
+
+/**
+ * \file configuration.h
+ * \brief Read interface for configuration files
+ */
+
+namespace libcamera {
+
+} /* namespace libcamera */
