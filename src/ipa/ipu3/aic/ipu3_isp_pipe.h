bf5d0c10ea214312a1a2fd3b3269f5dbe60f0371
Umang Jain <email@uajain.com>
2021-03-16 21:08:29 +0530

    ipa: ipu3: aic: Import IPU3ISPPipe
    It shall help us with KBL AIC wrapper

---
+++ src/ipa/ipu3/aic/ipu3_isp_pipe.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/*
+ * IPU3ISPPipe: Derived class from ISPPipe storing the AIC config
+ */
+
+#ifndef IPU3ISPPIPE_H
+#define IPU3ISPPIPE_H
+
+/* Included first to bring in our type wrapper */
+#include "kbl_aic.h"
+
+#include "IPU3AICCommon.h"
+#include "Pipe.h"
+
+namespace libcamera {
+
+#define NUM_ISP_PIPES 1
+
+typedef enum AicMode {
+	AIC_MODE_STILL = 0,
+	AIC_MODE_VIDEO,
+	AIC_MODE_MAX,
+} AicMode;
+
+class IPU3ISPPipe : public ISPPipe
+{
+public:
+	IPU3ISPPipe();
+	virtual ~IPU3ISPPipe();
+
+public:
+	// This function configures the HW/FW pipe via CSS interface
+	virtual void SetPipeConfig(const aic_output_t pipe_config);
+
+	virtual pipe_ver GetPipeVer() { return Czero; }
+
+	virtual const ia_aiq_rgbs_grid *GetAWBStats();
+	virtual const ia_aiq_af_grid *GetAFStats();
+	virtual const ia_aiq_histogram *GetAEStats();
+	virtual aic_config *GetAicConfig();
+
+	virtual void dump();
+
+private:
+	aic_output_t AicOutput;
+
+	aic_config AicConfig; /* Config to driver */
+};
+
+} /* namespace libcamera */
+
+#endif /* IPU3ISPPIPE_H */
