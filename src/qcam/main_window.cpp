55895961213559f780534c75e3b8285c93b65766
Kieran Bingham <kieran.bingham@ideasonboard.com>
2021-03-26 21:01:16 +0000

    qcam: MainWindow: Report hotplug events on statusBar
    Utilise the statusBar() to report hotplug events through our graphical view.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/qcam/main_window.cpp
+++ src/qcam/main_window.cpp
@@ -22,6 +22,7 @@
 #include <QInputDialog>
 #include <QMutexLocker>
 #include <QStandardPaths>
+#include <QStatusBar>
 #include <QStringList>
 #include <QTimer>
 #include <QToolBar>
@@ -621,7 +622,10 @@ void MainWindow::processHotplug(HotplugEvent *e)
 
 void MainWindow::addCamera(std::shared_ptr<Camera> camera)
 {
-	qInfo() << "Adding new camera:" << camera->id().c_str();
+	QString msg = QString("Adding new camera: ") + camera->id().c_str();
+	qInfo() << msg;
+	statusBar()->showMessage(msg, 2000);
+
 	QCoreApplication::postEvent(this,
 				    new HotplugEvent(std::move(camera),
 						     HotplugEvent::HotPlug));
@@ -629,7 +633,10 @@ void MainWindow::addCamera(std::shared_ptr<Camera> camera)
 
 void MainWindow::removeCamera(std::shared_ptr<Camera> camera)
 {
-	qInfo() << "Removing camera:" << camera->id().c_str();
+	QString msg = QString("Removing camera: ") + camera->id().c_str();
+	qInfo() << msg;
+	statusBar()->showMessage(msg, 2000);
+
 	QCoreApplication::postEvent(this,
 				    new HotplugEvent(std::move(camera),
 						     HotplugEvent::HotUnplug));
