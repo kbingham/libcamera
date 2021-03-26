cd985f742d13402a76b49e24f413a0e0aa8dcc8a
Kieran Bingham <kieran.bingham@ideasonboard.com>
2021-03-26 20:58:41 +0000

    qcam: MainWindow: Save JPG now
    Provide a saveImage function that will immediately save the current
    view-finder image using an ISODate formatted string, and report
    that string on the status bar.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/qcam/main_window.cpp
+++ src/qcam/main_window.cpp
@@ -16,6 +16,7 @@
 
 #include <QComboBox>
 #include <QCoreApplication>
+#include <QDate>
 #include <QFileDialog>
 #include <QImage>
 #include <QImageWriter>
@@ -215,6 +216,13 @@ int MainWindow::createToolbars()
 	action->setShortcut(Qt::Key_Space);
 	connect(action, &QAction::toggled, this, &MainWindow::toggleCapture);
 	startStopAction_ = action;
+
+	/* Fast save action */
+	action = toolbar_->addAction(QIcon::fromTheme("document-save",
+						      QIcon(":save.svg")),
+				     "Save");
+	action->setShortcut(QKeySequence::Save);
+	connect(action, &QAction::triggered, this, &MainWindow::saveImage);
 
 	/* Save As... action. */
 	action = toolbar_->addAction(QIcon::fromTheme("document-save-as",
@@ -645,6 +653,23 @@ void MainWindow::removeCamera(std::shared_ptr<Camera> camera)
 /* -----------------------------------------------------------------------------
  * Image Save
  */
+
+void MainWindow::saveImage()
+{
+	QImage image = viewfinder_->getCurrentImage();
+	QString defaultPath = QStandardPaths::writableLocation(QStandardPaths::PicturesLocation);
+
+	QDateTime now = QDateTime::currentDateTime();
+
+	QString filename = defaultPath + "/" + now.toString(Qt::ISODate) + ".jpg";
+
+	QImageWriter writer(filename);
+
+	statusBar()->showMessage("Saving" + filename, 2000);
+
+	writer.setQuality(95);
+	writer.write(image);
+}
 
 void MainWindow::saveImageAs()
 {
--- src/qcam/main_window.h
+++ src/qcam/main_window.h
@@ -62,6 +62,7 @@ private Q_SLOTS:
 	void switchCamera(int index);
 	void toggleCapture(bool start);
 
+	void saveImage();
 	void saveImageAs();
 	void captureRaw();
 	void processRaw(libcamera::FrameBuffer *buffer,
