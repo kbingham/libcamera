3440bbd321be70ac37e1a0d5f5e6a928c1a7d240
Kieran Bingham <kieran.bingham@ideasonboard.com>
2021-08-09 15:05:24 +0100

    Header clang format test
    Note - currently Z is sorting before a
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ alphabetical.cpp
@@ -0,0 +1,6 @@
+#include <c.h>
+#include <Z.h>
+#include <b.h>
+#include <z.h>
+#include <a.h>
+
---
+++ headers.cpp
@@ -0,0 +1,15 @@
+// Header checkers!
+
+#include <linux/media.h>
+#include <sys/mman.h>
+#include <libcamera/ipa/ipu3.h>
+#include <cmath>
+#include <vector>
+#include <string.h>
+#include <libcamera/base/private.h>
+#include "local.h"
+#include "libcamera/internal/framebuffer.h"
+#include "internalmodule/buffer.h"
+#include "headers.h"
+#include <libcamera/base/application.h>
+
