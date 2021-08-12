e3e3f0061056dd309751f4d5f6c5837500d67155
Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>
2021-08-12 08:51:06 +0200

    ipa: ipu3: Introduce a Context structure
    An increasing amount of data and information needs to be shared between
    the components that build up to implement image processing algorithms.
    
    Create a context structure which will allow us to work towards calling
    algorithms in a modular way, and sharing information between the modules.
    
    The IPA context is a global context set at configure time (IPAConfiguration)
    and a per-frame context (IPAFrameContext) used while streaming.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>

---
+++ src/ipa/ipu3/ipa_context.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2021, Google Inc.
+ *
+ * ipu3_ipa_context.h - IPU3 IPA Context
+ *
+ * Context information shared between the algorithms
+ */
+#ifndef __LIBCAMERA_IPU3_IPA_CONTEXT_H__
+#define __LIBCAMERA_IPU3_IPA_CONTEXT_H__
+
+#include <linux/intel-ipu3.h>
+
+namespace libcamera {
+
+namespace ipa::ipu3 {
+
+/* Fixed configuration of the IPA */
+struct IPAConfiguration {
+};
+
+/*
+ * Context of a frame for each algorithms
+ * This may be stored in a way that is associated with a given request
+ * lifetime, though for now a single instance is used.
+ */
+struct IPAFrameContext {
+};
+
+/* Global context of the IPA */
+struct IPAContext {
+	IPAConfiguration configuration;
+	IPAFrameContext frameContext;
+};
+
+} /* namespace ipa::ipu3 */
+
+} /* namespace libcamera*/
+
+#endif /* __LIBCAMERA_IPU3_IPA_CONTEXT_H__ */
