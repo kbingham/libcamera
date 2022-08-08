b50529c2ff090dde46a5ded5cab34e934ef2c623
Kieran Bingham <kieran.bingham@ideasonboard.com>
2022-08-08 23:20:20 +0100

    stash

--- src/ipa/ipu3/algorithms/shd.h
+++ src/ipa/ipu3/algorithms/shd.h
@@ -15,11 +15,21 @@ namespace ipa::ipu3::algorithms {
 
 class LensShadingCorrection : public Algorithm
 {
+	typedef struct componentData {
+		uint16_t ct;
+		std::vector<uint16_t> r;
+		std::vector<uint16_t> gr;
+		std::vector<uint16_t> gb;
+		std::vector<uint16_t> b;
+	} componentData_t;
+
 public:
 	LensShadingCorrection();
 
 	int init(IPAContext &context, const YamlObject &tuningData) override;
-	void prepare(IPAContext &context, ipu3_uapi_params *params) override;
+	void prepare(IPAContext &context, unsigned int frame,
+		     IPAFrameContext &frameContext,
+		     ipu3_uapi_params *params) override;
 
 private:
 	bool initialized_ = false;
@@ -29,13 +39,15 @@ private:
 	unsigned int gain_;
 	unsigned int x_, y_;
 
-	std::vector<uint16_t> rData_;
-	std::vector<uint16_t> grData_;
-	std::vector<uint16_t> gbData_;
-	std::vector<uint16_t> bData_;
+	std::vector<componentData_t> sets_;
 
-	std::vector<double> xSize_;
-	std::vector<double> ySize_;
+//	std::vector<uint16_t> rData_;
+//	std::vector<uint16_t> grData_;
+//	std::vector<uint16_t> gbData_;
+//	std::vector<uint16_t> bData_;
+
+//	std::vector<double> xSize_;
+//	std::vector<double> ySize_;
 };
 
 } /* namespace ipa::ipu3::algorithms */
