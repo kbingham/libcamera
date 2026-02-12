5a44c5af0649a0f29feaee0f9a2a1612fe4b4ac3
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2026-02-12 15:43:32 +0200

    libcamera: Use `it` variable name for iterators only
    Using the name `it` for a variable that is not an iterator is confusing.
    Give the variable a more explanatory name.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>

--- include/libcamera/internal/ipa_data_serializer.h
+++ include/libcamera/internal/ipa_data_serializer.h
@@ -114,12 +114,12 @@ public:
 		appendPOD<uint32_t>(dataVec, vecLen);
 
 		/* Serialize the members. */
-		for (auto const &it : data) {
+		for (const auto &value : data) {
 			std::vector<uint8_t> dvec;
 			std::vector<SharedFD> fvec;
 
 			std::tie(dvec, fvec) =
-				IPADataSerializer<V>::serialize(it, cs);
+				IPADataSerializer<V>::serialize(value, cs);
 
 			appendPOD<uint32_t>(dataVec, dvec.size());
 			appendPOD<uint32_t>(dataVec, fvec.size());
