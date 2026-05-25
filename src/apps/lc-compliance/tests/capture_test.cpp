a51f3831ed09fe3bc26ec1efde76ba454ed03800
Kieran Bingham <kieran.bingham@ideasonboard.com>
2026-05-25 11:11:31 +0200

    apps: lc-compliance: Reduce test repetition
    The lc-compliance test suite runs many combinations of tests with a
    variable number of requetss.
    
    This can validate that a pipeline handler succeeds with early startup
    and shutdown but having many different runs of the same test can be very
    slow.
    
    Reduce the set of NUMREQUESTS to continue validating the early (low
    count) tests and reduce the higher quantities which provide less value,
    but keep the highest as a more complete test.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/apps/lc-compliance/tests/capture_test.cpp
+++ src/apps/lc-compliance/tests/capture_test.cpp
@@ -114,7 +114,7 @@ TEST_P(SimpleCapture, UnbalancedStop)
 	capture.run(numRequests);
 }
 
-const int NUMREQUESTS[] = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+const int NUMREQUESTS[] = { 1, 2, 3, 5, 8, 89 };
 
 const std::vector<StreamRole> SINGLEROLES[] = {
 	{ StreamRole::Raw, },
