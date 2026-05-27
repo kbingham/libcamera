kbingham/libcamera

commit cc74afcdbd3ef9989a6f033c9fa52ad371e12626

libcamera: rpi: Make the controller min frame duration configurable
The controller min frame duration is used to rate limit how often we
run IPAs. Historically this has been set to 33333us, meaning that the
algorithms effectively skip frames when the camera is running faster
than 30fps.

This patch adds a small amount of plumbing that allows this value to
be set in the Raspberry Pi configuration file. Some applications or
platforms (such as Pi 5) are easily capable of running these more
often, should there be a need to do so.

Signed-off-by: David Plowman <david.plowman@raspberrypi.com>
Reviewed-by: Naushir Patuck <naush@raspberrypi.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/pipeline/rpi/common/pipeline_base.cpp
@@ -33,6 +33,12 @@ LOG_DEFINE_CATEGORY(RPI)
 
 using StreamFlag = RPi::Stream::StreamFlag;
 
+/*
+ * The IPA's algorithms will not be called more often than this many
+ * microseconds. The default corresponds to 30fps.
+ */
+constexpr float defaultControllerMinimumFrameDurationUs = 1000000.0 / 30.0;
+
 namespace {
 
 constexpr unsigned int defaultRawBitDepth = 12;
@@ -800,6 +806,12 @@ int PipelineHandlerBase::registerCamera(std::unique_ptr<RPi::CameraData> &camera
 	if (!data->sensor_)
 		return -EINVAL;
 
+	ret = data->loadPipelineConfiguration();
+	if (ret) {
+		LOG(RPI, Error) << "Unable to load pipeline configuration";
+		return ret;
+	}
+
 	/* Populate the map of sensor supported formats and sizes. */
 	for (const auto mbusCode : data->sensor_->mbusCodes())
 		data->sensorFormats_.emplace(mbusCode,
@@ -858,12 +870,6 @@ int PipelineHandlerBase::registerCamera(std::unique_ptr<RPi::CameraData> &camera
 	ret = platformRegister(cameraData, frontend, backend);
 	if (ret)
 		return ret;
-
-	ret = data->loadPipelineConfiguration();
-	if (ret) {
-		LOG(RPI, Error) << "Unable to load pipeline configuration";
-		return ret;
-	}
 
 	/* Setup the general IPA signal handlers. */
 	data->frontendDevice()->dequeueTimeout.connect(data, &RPi::CameraData::cameraTimeout);
@@ -1095,6 +1101,7 @@ int CameraData::loadPipelineConfiguration()
 {
 	config_ = {
 		.cameraTimeoutValue = 0,
+		.controllerMinFrameDurationUs = defaultControllerMinimumFrameDurationUs,
 	};
 
 	/* Initial configuration of the platform, in case no config file is present */
@@ -1144,6 +1151,9 @@ int CameraData::loadPipelineConfiguration()
 		frontendDevice()->setDequeueTimeout(config_.cameraTimeoutValue * 1ms);
 	}
 
+	config_.controllerMinFrameDurationUs =
+		phConfig["controller_min_frame_duration_us"].get<double>(config_.controllerMinFrameDurationUs);
+
 	return platformPipelineConfigure(root);
 }
 
@@ -1172,6 +1182,8 @@ int CameraData::loadIPA(ipa::RPi::InitResult *result)
 	}
 
 	params.lensPresent = !!sensor_->focusLens();
+	params.controllerMinFrameDurationUs = config_.controllerMinFrameDurationUs;
+
 	ret = platformInitIpa(params);
 	if (ret)
 		return ret;

include/libcamera/ipa/raspberrypi.mojom
@@ -18,6 +18,7 @@ struct SensorConfig {
 struct InitParams {
 	bool lensPresent;
 	libcamera.IPACameraSensorInfo sensorInfo;
+	float controllerMinFrameDurationUs;
 	/* PISP specific */
 	libcamera.SharedFD fe;
 	libcamera.SharedFD be;

src/ipa/rpi/common/ipa_base.cpp
@@ -46,14 +46,6 @@ constexpr Duration defaultExposureTime = 20.0ms;
 constexpr Duration defaultMinFrameDuration = 1.0s / 30.0;
 constexpr Duration defaultMaxFrameDuration = 250.0s;
 
-/*
- * Determine the minimum allowable inter-frame duration to run the controller
- * algorithms. If the pipeline handler provider frames at a rate higher than this,
- * we rate-limit the controller Prepare() and Process() calls to lower than or
- * equal to this rate.
- */
-constexpr Duration controllerMinFrameDuration = 1.0s / 30.0;
-
 /* List of controls handled by the Raspberry Pi IPA */
 const ControlInfoMap::Map ipaControls{
 	/* \todo Move this to the Camera class */
@@ -185,6 +177,14 @@ int32_t IpaBase::init(const IPASettings &settings, const InitParams &params, Ini
 		ctrlMap.merge(ControlInfoMap::Map(ipaColourControls));
 
 	result->controlInfo = ControlInfoMap(std::move(ctrlMap), controls::controls);
+
+	/*
+	 * This determines the minimum allowable inter-frame duration to run the
+	 * controller algorithms. If the pipeline handler provider frames at a
+	 * rate higher than this, we rate-limit the controller Prepare() and
+	 * Process() calls to lower than or equal to this rate.
+	 */
+	controllerMinFrameDuration_ = params.controllerMinFrameDurationUs * 1us;
 
 	return platformInit(params, result);
 }
@@ -467,7 +467,7 @@ void IpaBase::prepareIsp(const PrepareParams &params)
 	/* Allow a 10% margin on the comparison below. */
 	Duration delta = (frameTimestamp - lastRunTimestamp_) * 1.0ns;
 	if (lastRunTimestamp_ && frameCount_ > invalidCount_ &&
-	    delta < controllerMinFrameDuration * 0.9 && !hdrChange) {
+	    delta < controllerMinFrameDuration_ * 0.9 && !hdrChange) {
 		/*
 		 * Ensure we merge the previous frame's metadata with the current
 		 * frame. This will not overwrite exposure/gain values for the

src/ipa/rpi/common/ipa_base.h
@@ -142,6 +142,8 @@ private:
 	} flickerState_;
 
 	bool awbEnabled_;
+
+	utils::Duration controllerMinFrameDuration_;
 };
 
 } /* namespace ipa::RPi */

src/libcamera/pipeline/rpi/common/pipeline_base.h
@@ -169,6 +169,11 @@ public:
 		 * on frame durations.
 		 */
 		unsigned int cameraTimeoutValue;
+		/*
+		 * The minimum frame duration between the IPA's calls to the
+		 * algorithms themselves (in microseconds).
+		 */
+		float controllerMinFrameDurationUs;
 	};
 
 	Config config_;

src/libcamera/pipeline/rpi/pisp/data/example.yaml
@@ -36,5 +36,11 @@
                 # framebuffers required for its operation.
                 #
                 # "disable_hdr": false,
+
+                # Limits the rate at which IPAs are called. The algorithms will
+                # be skipped until this many microseconds have elapsed since
+                # the last call. The default value represents a 30fps limit.
+                #
+                # "controller_min_frame_duration_us": 33333.333,
         }
 }

src/libcamera/pipeline/rpi/vc4/data/example.yaml
@@ -37,5 +37,11 @@
                 # timeout value.
                 #
                 # "camera_timeout_value_ms": 0,
+
+                # Limits the rate at which IPAs are called. The algorithms will
+                # be skipped until this many microseconds have elapsed since
+                # the last call. The default value represents a 30fps limit.
+                #
+                # "controller_min_frame_duration_us": 33333.333,
         }
 }

commit ad5506544b798539e1d094ec2eac0b2a1ecbdc4d

pipeline: rpi: Fix crash when configuring timeout
When loading a config containing a nonzero "camera_timeout_value_ms"
(as is required for externally-triggered cameras) it would crash as
the pipeline handler tried to access both the IPA and frontEndDevice
before either had been created. Re-order to avoid the crash.

Fixes: cc74afcdbd3e ("Make the controller min frame duration configurable")

Signed-off-by: Nick Hollinghurst <nick.hollinghurst@raspberrypi.com>
Reviewed-by: Naushir Patuck <naush@raspberrypi.com>
Reviewed-by: David Plowman <david.plowman@raspberrypi.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/pipeline/rpi/common/pipeline_base.cpp
@@ -878,6 +878,16 @@ int PipelineHandlerBase::registerCamera(std::unique_ptr<RPi::CameraData> &camera
 	data->ipa_->setLensControls.connect(data, &CameraData::setLensControls);
 	data->ipa_->metadataReady.connect(data, &CameraData::metadataReady);
 
+	/*
+	 * Disable the IPA signal to control timeout, when there is a
+	 * user-requested value. We do this here now that the IPA and
+	 * front end device are both initialized.
+	 */
+	if (data->config_.cameraTimeoutValue) {
+		data->ipa_->setCameraTimeout.disconnect();
+		data->frontendDevice()->setDequeueTimeout(data->config_.cameraTimeoutValue * 1ms);
+	}
+
 	return 0;
 }
 
@@ -1144,12 +1154,6 @@ int CameraData::loadPipelineConfiguration()
 
 	config_.cameraTimeoutValue =
 		phConfig["camera_timeout_value_ms"].get<unsigned int>(config_.cameraTimeoutValue);
-
-	if (config_.cameraTimeoutValue) {
-		/* Disable the IPA signal to control timeout and set the user requested value. */
-		ipa_->setCameraTimeout.disconnect();
-		frontendDevice()->setDequeueTimeout(config_.cameraTimeoutValue * 1ms);
-	}
 
 	config_.controllerMinFrameDurationUs =
 		phConfig["controller_min_frame_duration_us"].get<double>(config_.controllerMinFrameDurationUs);