59683dfd2cbe6c0e5ad230fd8d6e32567952e042
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2026-06-30 03:06:33 +0300

    utils: Merge shader header generation scripts
    The shader header is generated by a combination of a shell script and a
    Python script. Simplify this by merging everything into a single script.
    
    The output of the old and new script only differ in white spaces.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Milan Zamazal <mzamazal@redhat.com>
    Reviewed-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ utils/gen-shader-headers.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2025, Bryan O'Donoghue
+# Copyright (C) 2026, Ideas on Board Oy
+#
+# Generate a C header file containing hex-encoded shader sources
+
+import argparse
+import binascii
+import math
+import pathlib
+import sys
+
+
+def process_file(path, out):
+    data = open(path, 'rb').read()
+
+    hex_data = [f'0x{c:02x}' for c in data]
+    var_name = path.name.replace('.', '_')
+
+    out.write(f'unsigned char const {var_name}[] = {{\n')
+
+    for i in range(math.ceil(len(data) / 16)):
+        out.write('\t')
+        out.write(', '.join(hex_data[16 * i:16 * (i + 1)]))
+        out.write(',\n')
+
+    out.write('};\n\n')
+    out.write(f'const unsigned int {var_name}_len = {len(data)};\n')
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description='Generate a C header file containing hex-encoded shader sources')
+    parser.add_argument('--output', '-o', metavar='file', default=sys.stdout,
+                        type=argparse.FileType('w', encoding='utf-8'),
+                        help='Output file name. Defaults to standard output if not specified.')
+    parser.add_argument('inputs', nargs='+', type=pathlib.Path,
+                        help='Input file names.')
+    args = parser.parse_args(argv[1:])
+
+    args.output.write('''\
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/* This file is auto-generated, do not edit! */
+
+#pragma once
+
+/*
+ * List the names of the shaders at the top of header for readability's sake.
+ *
+''')
+
+    for path in args.inputs:
+        name = path.name.replace('.', '_')
+        args.output.write(f' * unsigned char {name};\n')
+
+    args.output.write('''\
+ */
+
+/* Hex encoded shader data */
+''')
+
+    for path in args.inputs:
+        process_file(path, args.output)
+        args.output.write('\n')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
