kbingham/libcamera

commit d5b0cec83940b7965bc215383c247da25304ebca

libcamera: framebuffer: Turn the cookie into a 64-bit integer
To allow associating pointers with FrameBuffer instances, turn the
cookie into a 64-bit integer. This matches the Request cookie with is
also a uint64_t.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Umang Jain <umang.jain@ideasonboard.com>

include/libcamera/framebuffer.h
@@ -66,8 +66,8 @@ public:
 	Request *request() const;
 	const FrameMetadata &metadata() const { return metadata_; }
 
-	unsigned int cookie() const { return cookie_; }
-	void setCookie(unsigned int cookie) { cookie_ = cookie; }
+	uint64_t cookie() const { return cookie_; }
+	void setCookie(uint64_t cookie) { cookie_ = cookie; }
 
 	std::unique_ptr<Fence> releaseFence();
 
@@ -80,7 +80,7 @@ private:
 
 	FrameMetadata metadata_;
 
-	unsigned int cookie_;
+	uint64_t cookie_;
 };
 
 } /* namespace libcamera */

commit 849cd2dba7834009c1c6452b5586c469f9020f92

libcamera: framebuffer: Fix cookie arg type
The cookie has type `uint64_t`, but the constructor argument is of type
`unsigned int`. Fix that.

Unfortunately this is a public API and ABI change.

Fixes: d5b0cec83940 ("libcamera: framebuffer: Turn the cookie into a 64-bit integer")
Signed-off-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

include/libcamera/framebuffer.h
@@ -58,7 +58,7 @@ public:
 		unsigned int length;
 	};
 
-	FrameBuffer(std::span<const Plane> planes, unsigned int cookie = 0);
+	FrameBuffer(std::span<const Plane> planes, uint64_t cookie = 0);
 	FrameBuffer(std::unique_ptr<Private> d);
 	virtual ~FrameBuffer() {}
 

src/libcamera/framebuffer.cpp
@@ -309,7 +309,7 @@ ino_t fileDescriptorInode(const SharedFD &fd)
  * \param[in] planes The frame memory planes
  * \param[in] cookie Cookie
  */
-FrameBuffer::FrameBuffer(std::span<const Plane> planes, unsigned int cookie)
+FrameBuffer::FrameBuffer(std::span<const Plane> planes, uint64_t cookie)
 	: FrameBuffer(std::make_unique<Private>(planes, cookie))
 {
 }

src/py/libcamera/py_main.cpp
@@ -376,7 +376,7 @@ PYBIND11_MODULE(_libcamera, m)
 		});
 
 	pyFrameBuffer
-		.def(py::init<std::vector<FrameBuffer::Plane>, unsigned int>(),
+		.def(py::init<std::vector<FrameBuffer::Plane>, uint64_t>(),
 		     py::arg("planes"), py::arg("cookie") = 0)
 		.def_property_readonly("metadata", &FrameBuffer::metadata, py::return_value_policy::reference_internal)
 		.def_property_readonly("planes", [](const FrameBuffer &self) {